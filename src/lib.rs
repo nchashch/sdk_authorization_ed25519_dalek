16e16a2c5208d0d8690129c47eb47dd30b1b6fea
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2023-03-20 22:31:03 +0400

    Use just one Error type
    * Don't use SignatureError directly

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub use ed25519_dalek::{Keypair, PublicKey, Signature, SignatureError, Signer};
-use sdk_types::*;
+use sdk_types::{hash, Address, GetAddress, Transaction};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
@@ -20,7 +20,7 @@ pub fn get_address(public_key: &PublicKey) -> Address {
 
 pub fn verify_authorizations<C: Clone + Serialize>(
     transactions: &[Transaction<Authorization, C>],
-) -> Result<(), SignatureError> {
+) -> Result<(), Error> {
     let capacity: usize = transactions
         .iter()
         .map(|transaction| transaction.authorizations.len())
@@ -55,13 +55,13 @@ pub fn verify_authorizations<C: Clone + Serialize>(
 pub fn authorize<C: Clone + Serialize>(
     addresses_keypairs: &[(Address, &Keypair)],
     transaction: Transaction<Authorization, C>,
-) -> Result<Transaction<Authorization, C>, AuthorizationError> {
+) -> Result<Transaction<Authorization, C>, Error> {
     let mut authorizations: Vec<Authorization> = Vec::with_capacity(addresses_keypairs.len());
     let transaction_hash_without_authorizations = hash(&transaction);
     for (address, keypair) in addresses_keypairs {
         let hash_public_key = Address::from(hash(&keypair.public.to_bytes()));
         if *address != hash_public_key {
-            return Err(AuthorizationError::WrongKeypairForAddress {
+            return Err(Error::WrongKeypairForAddress {
                 address: *address,
                 hash_public_key,
             });
@@ -79,7 +79,7 @@ pub fn authorize<C: Clone + Serialize>(
 }
 
 #[derive(Debug, thiserror::Error)]
-pub enum AuthorizationError {
+pub enum Error {
     #[error(
         "wrong keypair for address: address = {address},  hash(public_key) = {hash_public_key}"
     )]
@@ -87,4 +87,6 @@ pub enum AuthorizationError {
         address: Address,
         hash_public_key: Address,
     },
+    #[error("ed25519_dalek error")]
+    DalekError(#[from] SignatureError),
 }
